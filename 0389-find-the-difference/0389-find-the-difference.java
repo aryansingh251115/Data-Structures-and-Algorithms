e3cf2f3ace9421d516ba2956b6f3ed06435528e5
aryansingh251115 <145383803+aryansingh251115@users.noreply.github.com>
2024-09-15 23:58:58 +0530

    Time: 3 ms (45.14%), Space: 41.4 MB (95.39%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,23 +1,44 @@
+// //help taken for developing approach of question
+
+// //using hashset
+
+// class Solution {
+//     public char findTheDifference(String s, String t) {
+//         HashMap<Character, Integer> hs = new HashMap<>();
+//         for (int i = 0; i < s.length(); i++) {
+//             char c = s.charAt(i);
+//             hs.put(c, hs.getOrDefault(c, 0) + 1);
+//         }
+//         // Searching the element in the map
+//         for (int i = 0; i < t.length(); i++) {
+//             char d = t.charAt(i);
+//             if (!hs.containsKey(d)) {
+//                 return d;
+//             } 
+//             else {
+//                 hs.put(d, hs.get(d)-1);
+//                 if (hs.get(d) == 0) {
+//                     hs.remove(d);
+//                 }
+//             }
+//         }
+//     return ' ';
+//     }
+// }
+
+//using XOR operations
+
 class Solution {
     public char findTheDifference(String s, String t) {
-        HashMap<Character, Integer> hs = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            hs.put(c, hs.getOrDefault(c, 0) + 1);
+        char result = 0;
+        for(int i = 0;i<s.length();i++){
+            result ^=s.charAt(i);
         }
-        // Searching the element in the map
-        for (int i = 0; i < t.length(); i++) {
-            char d = t.charAt(i);
-            if (!hs.containsKey(d)) {
-                return d;
-            } 
-            else {
-                hs.put(d, hs.get(d)-1);
-                if (hs.get(d) == 0) {
-                    hs.remove(d);
-                }
-            }
+
+        for(int i = 0;i<t.length();i++){
+            result ^=t.charAt(i);
         }
-    return ' ';
+        return result;
+        
     }
 }
