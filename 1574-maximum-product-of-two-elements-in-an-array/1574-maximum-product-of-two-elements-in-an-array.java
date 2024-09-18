7fb46ebc6c23d14bfb577d22de380fe1c3824489
aryansingh251115 <145383803+aryansingh251115@users.noreply.github.com>
2024-09-18 20:10:21 +0530

    Time: 2 ms (64.41%), Space: 42.7 MB (53.06%) - LeetHub

--- 1574-maximum-product-of-two-elements-in-an-array/1574-maximum-product-of-two-elements-in-an-array.java
+++ 1574-maximum-product-of-two-elements-in-an-array/1574-maximum-product-of-two-elements-in-an-array.java
@@ -1,3 +1,7 @@
+//TC - O(nlog n)
+//From other approaches we can do this by linear TC as well.
+
+
 //done without help
 class Solution {
     public int maxProduct(int[] nums) {
@@ -5,3 +9,19 @@ class Solution {
         return ((nums[nums.length-1]-1) * (nums[nums.length-2]-1));
     }
 }
+
+// public class Solution {
+//     public int maxProduct(int[] nums) {
+//         int n = nums.length;
+
+//         int maxNum = nums[0];
+//         int result = 0;
+
+//         for (int i = 1; i < n; i++) {
+//             result = Math.max(result, (maxNum - 1) * (nums[i] - 1));
+//             maxNum = Math.max(maxNum, nums[i]);
+//         }
+
+//         return result;
+//     }
+// }
