bbd21ff210cee2e2441647e1439573a5849190fb
aryansingh251115 <145383803+aryansingh251115@users.noreply.github.com>
2024-09-21 00:16:32 +0530

    Time: 3 ms (87.04%), Space: 45.1 MB (41.44%) - LeetHub

--- 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.java
+++ 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.java
@@ -1,6 +1,38 @@
+//my approach but having TC - O(n log n)
+// class Solution {
+//     public int maxProductDifference(int[] nums) {
+//         Arrays.sort(nums);
+//         return nums[nums.length-1]*nums[nums.length-2] - nums[0]*nums[1];
+//     }
+// }
+
+
+//mike's approach
 class Solution {
     public int maxProductDifference(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length-1]*nums[nums.length-2] - nums[0]*nums[1];
+        int largest = Integer.MIN_VALUE;
+        int largest2 = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+        int smallest2 = Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>largest){
+                largest2 = largest;
+                largest = nums[i];
+
+            }
+            else  if(nums[i]>largest2){
+                largest2 = nums[i];
+            }
+            if(nums[i] < smallest){
+                smallest2 = smallest;
+                smallest = nums[i];
+            }
+            else if(nums[i]<smallest2){
+                
+                smallest2 = nums[i];
+            }
+        }
+        return largest*largest2 - smallest*smallest2;
+
     }
 }
