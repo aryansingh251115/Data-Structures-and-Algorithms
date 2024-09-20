27302d5fcd9f71d70141c117353e513a9cd21b32
aryansingh251115 <145383803+aryansingh251115@users.noreply.github.com>
2024-09-21 00:43:21 +0530

    Time: 2 ms (75.54%), Space: 44.9 MB (7.34%) - LeetHub

--- 2756-buy-two-chocolates/2756-buy-two-chocolates.java
+++ 2756-buy-two-chocolates/2756-buy-two-chocolates.java
@@ -1,9 +1,36 @@
+//my approach - O(n logn)
+
+// class Solution {
+//     public int buyChoco(int[] prices, int money) {
+//         Arrays.sort(prices);
+//         int total = prices[0] + prices[1];
+//         if(total <= money){
+//             return money- total;
+//         }
+//         else{
+//             return money;
+//         }
+//     }
+// }
+
+
+//My another approach
+
 class Solution {
     public int buyChoco(int[] prices, int money) {
-        Arrays.sort(prices);
-        int total = prices[0] + prices[1];
-        if(total <= money){
-            return money- total;
+        int smallest = Integer.MAX_VALUE;
+        int smallest2 = Integer.MAX_VALUE;
+        for(int i =0 ;i<prices.length;i++){
+            if(prices[i]<smallest){
+                smallest2 = smallest;
+                smallest = prices[i];
+            }
+            else if(prices[i]<smallest2){
+                smallest2 = prices[i];
+            }
+        }
+        if(smallest +smallest2 <= money){
+            return money - smallest -smallest2;
         }
         else{
             return money;
