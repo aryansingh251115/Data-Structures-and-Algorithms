d097de4062a1895a3214e0c46493a9696cf791cd
aryansingh251115 <145383803+aryansingh251115@users.noreply.github.com>
2024-09-26 00:06:58 +0530

    Time: 15 ms (11.44%), Space: 44.3 MB (25.18%) - LeetHub

--- 2025-redistribute-characters-to-make-all-strings-equal/2025-redistribute-characters-to-make-all-strings-equal.java
+++ 2025-redistribute-characters-to-make-all-strings-equal/2025-redistribute-characters-to-make-all-strings-equal.java
@@ -1,22 +1,35 @@
+// Done by myself
 class Solution {
     public boolean makeEqual(String[] words) {
-        HashMap<Character,Integer> hm = new HashMap<>();
-        for(String s : words){
-            for(int i = 0 ; i < s.length();i++){
-                char c = s.charAt(i);
-                if(hm.containsKey(c)){
-                    hm.put(c,hm.get(c)+1);
-                }
-                else{
-                    hm.put(c,1);
+        // HashMap to store the frequency of each character across all words
+        HashMap<Character, Integer> hm = new HashMap<>();
+        
+        // Loop through each word in the array
+        for (String s : words) {
+            // Loop through each character in the current word
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i); // Get the current character
+                
+                // If the character already exists in the map, increment its count
+                if (hm.containsKey(c)) {
+                    hm.put(c, hm.get(c) + 1);
+                } 
+                // If the character does not exist in the map, add it with a count of 1
+                else {
+                    hm.put(c, 1);
                 }
             }
         }
-        for(Character d : hm.keySet()){
-            if(hm.get(d) %  words.length != 0){
+        
+        // Check if the frequency of each character is divisible by the number of words
+        for (Character d : hm.keySet()) {
+            // If the frequency of any character is not divisible by the number of words, return false
+            if (hm.get(d) % words.length != 0) {
                 return false;
             }
         }
-    return true;
+        
+        // If all character frequencies are divisible by the number of words, return true
+        return true;
     }
 }
