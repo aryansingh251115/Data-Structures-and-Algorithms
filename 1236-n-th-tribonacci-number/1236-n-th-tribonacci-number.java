38138eb6585a8789226d068115dd41dc5108717a
aryansingh251115 <145383803+aryansingh251115@users.noreply.github.com>
2024-10-21 01:08:12 +0530

    Time: 0 ms (100%), Space: 40.2 MB (51.59%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -1,21 +1,53 @@
-//Approach 1
+//Approach 3
+//Try with Dp
+
+//Approach 4 - Recursion 
+
 class Solution {
     public int tribonacci(int n) {
-        if(n == 1 || n == 2) return 1;
-        if(n == 0) return 0;
-        int a = 0;
-        int b = 1;
-        int c = 1;
-        int d = 0;
-        for (int i = 3; i <= n; i++) {
-            d = a + b + c;
-            a = b;
-            b = c;
-            c = d;
+        int memo[] = new int[n + 1];
+        for (int i = 0; i <= n ; i++) {
+            memo[i] = -1;
         }
-    return d;
+        return helper(n, memo);
+    }
+
+    private int helper(int n, int memo[]) {
+        if (n == 0)
+            return 0;
+        if (n == 1 || n == 2)
+            return 1;
+
+        if (memo[n] != -1)
+            return memo[n];
+
+        memo[n] = helper(n - 1, memo) + helper(n - 2, memo) + helper(n - 3, memo);
+        return memo[n];
     }
 }
+
+// Approach 1
+// class Solution {
+// public int tribonacci(int n) {
+// if (n == 1 || n == 2)
+// return 1;
+// if (n == 0)
+// return 0;
+// int a = 0;
+// int b = 1;
+// int c = 1;
+// int d = 0;
+// for (int i = 3; i <= n; i++) {
+// d = a + b + c;
+// a = b;
+// b = c;
+// c = d;
+// }
+// return d;
+// }
+// }
+
+// Approach 2
 
 // done by using hint 1 but without any other help
 
