62cb15275508527a67120532655ea1ae234094f2
aryansingh251115 <145383803+aryansingh251115@users.noreply.github.com>
2024-10-21 00:31:07 +0530

    Time: 0 ms (100%), Space: 40.7 MB (5.8%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -1,12 +1,33 @@
+//Approach 1
 class Solution {
     public int tribonacci(int n) {
-        int arr[] = new int[38];
-        arr[0] = 0;
-        arr[1] = 1;
-        arr[2] = 1;
-        for(int i = 3;i <=37;i++){
-            arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
+        if(n == 1 || n == 2) return 1;
+        if(n == 0) return 0;
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int d = 0;
+        for (int i = 3; i <= n; i++) {
+            d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
         }
-        return arr[n];
+    return d;
     }
 }
+
+// done by using hint 1 but without any other help
+
+// class Solution {
+// public int tribonacci(int n) {
+// int arr[] = new int[38];
+// arr[0] = 0;
+// arr[1] = 1;
+// arr[2] = 1;
+// for(int i = 3;i <=37;i++){
+// arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
+// }
+// return arr[n];
+// }
+// }
