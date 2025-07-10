1cdee638c71575a68c99730041d58acba4ca7368
aryansingh251115 <145383803+aryansingh251115@users.noreply.github.com>
2025-07-10 22:38:00 +0530

    Time: 11 ms (38.17%), Space: 49.1 MB (8.98%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,27 +1,21 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        int count = 0; // To count the number of content children
-        
-        // Sort both arrays to try to satisfy the children with the smallest greed factor first
-        Arrays.sort(g); // Sort the greed factor array (children's greed levels)
-        Arrays.sort(s); // Sort the size array (cookie sizes)
-
-        int i = 0; // Pointer for cookies
-        int j = 0; // Pointer for children
-
-        // Iterate while both the cookies and children arrays have unprocessed elements
-        while (i < s.length && j < g.length) {
-            // If the current cookie can satisfy the current child
-            if (s[i] >= g[j]) {
-                count++; // Increase the count of content children
-                i++; // Move to the next cookie
-                j++; // Move to the next child
-            } else {
-                // If the current cookie can't satisfy the current child, try the next larger cookie
-                i++;
+        int ans = 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int idxG = 0;
+        int idxS = 0;
+        while(idxG <g.length && idxS <s.length){
+            if(g[idxG] <= s[idxS]){
+                ans++;
+                idxG++;
+                idxS++;
+            }
+            else if(g[idxG] > s[idxS]){
+                idxS++;
             }
         }
+        return ans;
 
-        return count; // Return the total number of content children
     }
 }
