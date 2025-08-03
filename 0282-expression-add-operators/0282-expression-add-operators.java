6f2d5e649a643572f66ee2f82583d3ef9b19bfc5
aryansingh251115 <145383803+aryansingh251115@users.noreply.github.com>
2025-08-04 00:45:27 +0530

    Time: 113 ms (33.79%), Space: 45.6 MB (53.53%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.java
+++ 0282-expression-add-operators/0282-expression-add-operators.java
@@ -1,33 +1,42 @@
 class Solution {
     public List<String> addOperators(String num, int target) {
-        List<String> lst = new ArrayList<>();
-        helper(lst, num, target, 0, "", 0, 0);
-        return lst;
+        // Map<String,Integer>  hm = new HashMap<>();
+        // hm.put('+' , 1);
+        // hm.put('-' , 2);
+        // hm.put('*' , 3);
+        List<String> ans = new ArrayList<>();
+        helper(0, target, num,ans , "" , 0,0);
+        return ans;
     }
 
-    private void helper(List<String> lst, String num, int target, int idx, String temp, long numTemp, long prev) {
+    private void helper(int idx, int target, String num, List<String> ans, String temp,long tempAns, long prev) {
         if (idx == num.length()) {
-            if (numTemp == target) {
-                lst.add(temp);
+            if (tempAns == target) {
+                ans.add(temp);
             }
             return;
         }
 
         for (int i = idx; i < num.length(); i++) {
-            // Skip numbers with leading zeros
-            if (i != idx && num.charAt(idx) == '0') break;
+            if (i != idx && num.charAt(idx) == '0')
+                break;
 
             String currStr = num.substring(idx, i + 1);
-            long val = Long.parseLong(currStr);
+            long numAns = Long.parseLong(currStr);
 
             if (idx == 0) {
-                // First number, so no operator
-                helper(lst, num, target, i + 1, currStr, val, val);
+                helper(i + 1, target, num, ans, currStr , numAns,numAns);
+
             } else {
-                helper(lst, num, target, i + 1, temp + "+" + currStr, numTemp + val, val);
-                helper(lst, num, target, i + 1, temp + "-" + currStr, numTemp - val, -val);
-                helper(lst, num, target, i + 1, temp + "*" + currStr, numTemp - prev + prev * val, prev * val);
+                helper(i + 1, target, num, ans, temp + '+' + currStr, tempAns + numAns,numAns);
+                helper(i + 1, target, num, ans, temp + '-' + currStr, tempAns - numAns,-numAns);
+                helper(i + 1, target, num, ans, temp + '*' + currStr , tempAns - prev + prev * numAns,prev*numAns);
+
             }
+
+           
+
         }
+
     }
 }
